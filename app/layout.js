3709cf69792369dd380dedfce210144bdecac859
Autumn Leaf <82756899+ShubhamPaliwal157@users.noreply.github.com>
2026-03-09 23:36:47 +0530

    moving data to public

--- app/layout.js
+++ app/layout.js
@@ -21,7 +21,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${ibmPlexMono.variable} ${bebasNeue.variable}`}>
+      <body className={`${ibmPlexMono.variable} ${bebasNeue.variable}`} suppressHydrationWarning>
         {children}
       </body>
     </html>
